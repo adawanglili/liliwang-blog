9503aa96f61f12f2346f93316a64527d70067c88
adawanglili <adawanglili@gmail.com>
2026-09-08 21:34:58 -0400

    Widen post descriptions on homepage and update canonical site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from '@astrojs/cloudflare';
 import { defineConfig, fontProviders } from 'astro/config';
 
 export default defineConfig({
-	site: 'https://blog.adawanglili.workers.dev',
+	site: 'https://adawanglili.com',
 	output: 'static',
 	adapter: cloudflare({ imageService: 'compile' }),
 	integrations: [mdx(), sitemap()],
